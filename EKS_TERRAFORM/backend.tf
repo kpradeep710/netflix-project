ec36de910e78b991118de5bbb872a7799eacbe3f
kpradeep710 <128368784+kpradeep710@users.noreply.github.com>
2025-04-28 19:49:00 +0530

    Delete EKS_TERRAFORM directory

--- EKS_TERRAFORM/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "devsecops-netflix-reyaz" # Replace with your actual S3 bucket name
-    key    = "EKS/terraform.tfstate"
-    region = "ap-south-1"
-  }
-}
